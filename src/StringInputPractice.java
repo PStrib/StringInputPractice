a04370ed723fd730b689aa54afc81b1b2c4d57a7
PStrib <peter.strib@gmail.com>
2022-10-10 21:26:02 +0100

    Makes the program function up to spec

--- src/StringInputPractice.java
+++ src/StringInputPractice.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class StringInputPractice {
     public static void main(String[] args) {
         Scanner scan =new Scanner (System.in);
-        System.out.println("Enter two integers seperated by one or more spaces");
-        int a=scan.nextInt();
-        int b=scan.nextInt();
-        System.out.println("The larger integer is "+Math.max(a,b));
+        System.out.println("Enter a string of at least two letters");
+        String s=scan.nextLine();
+        System.out.println("String s is "+s);
+        System.out.println("The length of string s is "+s.length());
+        String firstHalf=s.substring(0,s.length()/2);
+        String secondHalf=s.substring(s.length()/2);
+        System.out.println(secondHalf+"."+firstHalf);
     }
 }
